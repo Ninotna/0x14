f7cc952c36eedb63c28169c37b0f7e1ca9367b3f
ninotna <antonin@dev87.fr>
2025-07-03 12:45:54 +0200

    Mise en place du routage avec react-router-dom entre ajout et liste des employés

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import EmployeeList from './pages/EmployeeList.jsx';
+import store from './redux/store';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/employees" element={<EmployeeList />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
